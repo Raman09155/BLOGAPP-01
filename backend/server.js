d9c05f4f43a0f989c35ecfd9864c14c6b870a7f0
prakashrajput330 <prakashkhushi330@gmail.com>
2025-10-06 15:30:48 +0530

    ready to diployement

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,12 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.NODE_ENV === 'production'
+        ? "https://airconnect-blog-app.onrender.com"
+        :"http://localhost:5173",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
     })
 );
 
